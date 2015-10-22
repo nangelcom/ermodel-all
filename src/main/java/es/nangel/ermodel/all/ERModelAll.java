e65c221034e1351c51b05d45dc9ffcfabb1982b1
Ángel Luis López Monterroso <angel9484@gmail.com>
2015-10-22 21:39:07 +0200

    Includes all and make a bridge with the run with base.

---
+++ src/main/java/es/nangel/ermodel/all/ERModelAll.java
@@ -0,0 +1,12 @@
+package es.nangel.ermodel.all;
+
+import es.nangel.ermodel.base.ERModelBase;
+
+/**
+ * Created by nangel on 22/10/15.
+ */
+public class ERModelAll {
+    public static void main(final String args[]) throws Exception {
+        ERModelBase.main(args);
+    }
+}
